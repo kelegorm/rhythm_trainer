5de62c98904b862c48e1cca7e8491b2e50c2f140
dmitry <dmitry@bassg.com>
2025-04-02 13:41:09 +0200

    Added meta and immutable for some classes

--- lib/drum_pattern.dart
+++ lib/drum_pattern.dart
@@ -1,17 +1,23 @@
+import 'package:meta/meta.dart';
+
+@immutable
 class DrumPattern {
   final int barsCount;
   final Iterable<DrumNote> notes;
 
-  DrumPattern(this.notes, this.barsCount) : assert(barsCount > 0);
+  const DrumPattern(this.notes, this.barsCount) : assert(barsCount > 0);
 }
 
+
+@immutable
 class DrumNote {
   final DrumPad pad;
   /// note start time in beats.
   final double startTime;
 
-  DrumNote(this.pad, this.startTime);
+  const DrumNote(this.pad, this.startTime);
 }
+
 
 enum DrumPad {
   left(id: 0),
